54d670f3bc272d143d02b32ba3340345b8c353c4
Andrei Ungureanu <andrei828>
2019-09-25 11:07:07 +0300

    bug fixes

--- New auth/config/database.js
+++ New auth/config/database.js
@@ -2,5 +2,4 @@
 module.exports = {
 
     //'url' : 'mongodb://localhost/passport' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'url' : 'mongodb://cosmos-db-mongodb:bgY0JHoj9sdvuS3BaVOLplVU0baxDR9G8ljg57lk5ogZOIc8FPcW0SvfsjhqyCgJC9z2LgWeEhATEdGHzdPufQ==@cosmos-db-mongodb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb'
 };
